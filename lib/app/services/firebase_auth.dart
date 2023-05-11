c84e7457eaad826ca79511a0188ce83fd43ce1db
Kaviraghul <vettrithamil@gmail.com>
2023-05-11 13:02:20 +0530

    firebase setup completed

---
+++ lib/app/services/firebase_auth.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+class Firebase {
+  static Future<UserCredential> signIn(String email, String password) async {
+    return await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  static CollectionReference getContactsCollection(String uid) {
+    return _firestore.collection('users').doc(uid).collection('contacts');
+  }
+}
